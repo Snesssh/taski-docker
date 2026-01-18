5bf0da43363dca86da5a121d4a041c85b9ff069f
snesssh <petin321@gmai.com>
2026-01-18 07:35:20 +0000

    tests added4

--- backend/api/tests/tests.py
+++ backend/api/tests/tests.py
@@ -1,12 +1,14 @@
 from http import HTTPStatus
 
 from api import models
+
 from django.test import Client, TestCase
+
 
 class TaskiApiTestCase(TestCase):
     def SetUp(self):
         self.guest_client - Client()
-    
+
     def self_list_exists(self):
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
@@ -16,3 +18,4 @@ class TaskiApiTestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertEqual(models.Task.objects.filter(title='Test').exists())
+
